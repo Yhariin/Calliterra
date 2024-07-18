9f9ff25b8b0c5549206f1646fc0e23cc2f398e4c
Yhariin <suarezcristian137@gmail.com>
2024-07-18 00:35:20 -0400

    Fixed bug where bufferLayoutArray was not being properly set.

--- Calliterra/src/Platform/DX11/DX11VertexBuffer.h
+++ Calliterra/src/Platform/DX11/DX11VertexBuffer.h
@@ -2,17 +2,17 @@
 #include "DX11Context.h"
 #include "Renderer/VertexBuffer.h"
 #include "Renderer/VertexBufferLayout.h"
+#include "Platform/DX11/DX11Shader.h"
 
 class DX11VertexBuffer : public VertexBuffer
 {
 public:
-	DX11VertexBuffer(DX11Context& context, float* vertices, uint32_t elementCount);
-	DX11VertexBuffer(DX11Context& context, float** listOfVertexArrays, uint32_t* listOfElementCounts, uint32_t bufferCount);
+	DX11VertexBuffer(DX11Context& context, float* vertices, uint32_t elementCount, ComPtr<ID3DBlob> shaderByteCode);
+	DX11VertexBuffer(DX11Context& context, float** listOfVertexArrays, uint32_t* listOfElementCounts, uint32_t bufferCount, ComPtr<ID3DBlob> vertexShader);
 
 	void Bind() override;
 	void Unbind() override;
 
-	//void SetLayout(const VertexBufferLayout& layout) override { m_D3DBufferLayoutList[0] = layout; }
 	void SetLayout(int index) override;
 	void CreateLayout(const VertexBufferLayout& layout) override;
 	void CreateLayoutList(const std::vector<VertexBufferLayout>& layoutList) override;
@@ -20,15 +20,14 @@ public:
 private:
 	DX11Context& m_DX11Context;
 
-	//std::vector<ComPtr<ID3D11Buffer>> m_D3DVertexBufferList;
-	//std::vector<ComPtr<ID3D11InputLayout>> m_D3DBufferLayoutList;
-	//std::vector<VertexBufferLayout> m_BufferLayoutList;
 	float** m_VertexArrayList;
 	uint32_t * m_ElementCountList;
 	uint32_t m_BufferCount;
 
-	ComPtr<ID3D11Buffer> m_D3DVertexBufferArray[D3D11_IA_VERTEX_INPUT_RESOURCE_SLOT_COUNT] = {};
-	ComPtr<ID3D11InputLayout> m_D3DBufferLayoutArray[D3D11_IA_VERTEX_INPUT_RESOURCE_SLOT_COUNT] = {};
-	VertexBufferLayout m_BufferLayoutArray[D3D11_IA_VERTEX_INPUT_RESOURCE_SLOT_COUNT] = {};
+	ComPtr<ID3D11Buffer> m_D3DVertexBufferArray[D3D11_IA_VERTEX_INPUT_RESOURCE_SLOT_COUNT];
+	ComPtr<ID3D11InputLayout> m_D3DBufferLayoutArray[D3D11_IA_VERTEX_INPUT_RESOURCE_SLOT_COUNT];
+	VertexBufferLayout m_BufferLayoutArray[D3D11_IA_VERTEX_INPUT_RESOURCE_SLOT_COUNT];
+
+	ComPtr<ID3DBlob> m_ShaderByteCode;
 };
 
