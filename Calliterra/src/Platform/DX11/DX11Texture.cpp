641c8ced28e5e7839987b3bd7b982a7eaca3f245
Yhariin <suarezcristian137@gmail.com>
2024-08-23 15:01:06 -0400

    Added mipmapping

--- Calliterra/src/Platform/DX11/DX11Texture.cpp
+++ Calliterra/src/Platform/DX11/DX11Texture.cpp
@@ -13,29 +13,31 @@ DX11Texture::DX11Texture(DX11Context& context, const std::string& filepath, uint
 	D3D11_TEXTURE2D_DESC textureDesc = {};
 	textureDesc.Width = m_Width;
 	textureDesc.Height = m_Height;
-	textureDesc.MipLevels = 1;
+	textureDesc.MipLevels = 0; // 0 will generate all mip levels
 	textureDesc.ArraySize = 1;
 	textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	textureDesc.SampleDesc.Count = 1;
 	textureDesc.SampleDesc.Quality = 0;
 	textureDesc.Usage = D3D11_USAGE_DEFAULT;
-	textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
 	textureDesc.CPUAccessFlags = 0;
-	textureDesc.MiscFlags = 0;
+	textureDesc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
  
-	D3D11_SUBRESOURCE_DATA sd = {};
-	sd.pSysMem = m_TextureData;
-	sd.SysMemPitch = m_Width * m_DesiredChannels;
+	//D3D11_SUBRESOURCE_DATA sd = {};
+	//sd.pSysMem = m_TextureData;
+	//sd.SysMemPitch = m_Width * m_DesiredChannels;
 
 	ASSERT_HR(
-		m_DX11Context.GetDevice().CreateTexture2D(&textureDesc, &sd, &m_Texture)
+		m_DX11Context.GetDevice().CreateTexture2D(&textureDesc, nullptr, &m_Texture)
 	);
+
+	m_DX11Context.GetDeviceContext().UpdateSubresource(m_Texture.Get(), 0, nullptr, m_TextureData, m_Width * m_DesiredChannels, 0);
 
 	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 	srvDesc.Format = textureDesc.Format;
 	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 	srvDesc.Texture2D.MostDetailedMip = 0;
-	srvDesc.Texture2D.MipLevels = 1;
+	srvDesc.Texture2D.MipLevels = -1; // -1 will use all mip levels
 
 	ASSERT_HR(
 		m_DX11Context.GetDevice().CreateShaderResourceView(m_Texture.Get(), &srvDesc, &m_TextureView)
@@ -46,11 +48,15 @@ DX11Texture::DX11Texture(DX11Context& context, const std::string& filepath, uint
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.MipLODBias = 0.f;
+	samplerDesc.MinLOD = 0.f;
+	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
 
 	ASSERT_HR(
 		m_DX11Context.GetDevice().CreateSamplerState(&samplerDesc, &m_SamplerState)
 	);
 
+	m_DX11Context.GetDeviceContext().GenerateMips(m_TextureView.Get());
 }
 
 void DX11Texture::Bind(std::optional<uint32_t> slot)
